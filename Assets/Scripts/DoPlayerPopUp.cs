813b50d260a0e90708764a2778c3464898859438
Rakesh Baruah <rbaruah@gmail.com>
2018-08-15 12:11:28 -0500

    second commit

---
+++ Assets/Scripts/DoPlayerPopUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoPlayerPopUp : MonoBehaviour
+{
+
+    
+
+    // Use this for initialization
+    private void Start()
+    {
+        this.enabled = false;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DoTogglePopUp();
+	}
+
+    void DoTogglePopUp()
+    {
+        if (this.enabled == false)
+        {
+            this.enabled = true;
+        }
+        else
+        {
+           this.enabled = false;
+        }
+    }
+}
